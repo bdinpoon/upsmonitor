28dc5d0f87b92f619fac8732dbe8be7f4352d61e
bdinpoon <mfair55@gmail.com>
2019-04-12 22:42:13 -0400

    Update install.sh
    Found a bug in install.sh, I'm assuming it was due to "smart quotes" as this was originally copied from the machine to Windows...

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#Install Script for upsmonitor service
+#Install Script for upsmonitor service 
 #Written by BarryDinpoon
 
 mv upsmonitor /usr/bin/upsmonitor
 chmod +x /usr/bin/upsmonitor
 
 mv upsmonitor.service /etc/systemd/system/upsmonitor.service
-systemctl daemon-reload
+systemctl deamon-reload
+systemctl enable upsmonitor
+systemctl start upsmonitor
 
-echo "upsmonitor is now installed. You may start the service by running systemctl start upsmonitor.service"
-echo "***NOTE*** If you have not edited your InfluxDB information into upsmonitor script you will need to do that now!"
-echo "***NOTE*** If you have not installed PowerPanel yet you will need to do that now otherwise this will not work."
+echo "upsmonitor is now instaslled. The service has been started automatically."
+echo "***NOTE*** If you haven't edited the script to include your InfluxDB information you will need to restart the service."
+echo "***NOTE*** If you have not installed PowerPanel yet you will need to do that before this will work."
+
+
+
+
